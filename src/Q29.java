fe105096944ac61986aeab7883d697d9a9feeac7
Kaivalaya Dua <kevindua26@gmail.com>
2024-09-04 18:35:14 +0530

    Corrected error in Q29

--- src/Q29.java
+++ src/Q29.java
@@ -1,17 +1,17 @@
 public class Q29 {
 /*
     Write a java program to print the pattern using nested for loop.
-    *
-    **
-    ***
-    ****
     *****
+    ****
+    ***
+    **
+    *
 */
     public static void main(String[] args) {
         int n = 5;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = i; j <= n; j++) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
                 System.out.print("*");
             }
             System.out.println();
